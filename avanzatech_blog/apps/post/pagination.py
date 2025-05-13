114229f1a2905ac2c3edadf7e690426d4f120919
danilo <danilo1000411174@gmail.com>
2025-05-13 18:04:41 -0500

    PAGINACION

--- avanzatech_blog/apps/post/pagination.py
+++ avanzatech_blog/apps/post/pagination.py
@@ -3,18 +3,20 @@ from rest_framework.response import Response
 import math
 
 class CustomPagination(PageNumberPagination):
-    page_size = 10                                      #Elements per page
+    page_size = 10                                      
     page_size_query_param = "page_size"
     max_page_size = 100
 
     def get_paginated_response(self, data):
         total_pages = math.ceil(self.page.paginator.count / self.page_size)
         return Response({
+            'pagination':{      
             "total_count": self.page.paginator.count,    
             "total_pages": total_pages,                  
-            "current_page": self.page.number,            
-            "next": self.get_next_link(),
-            "previous": self.get_previous_link(),
+            "current_page": self.page.number,     
+            "first_elem": self.page.start_index(),       
+            "last_elem": self.page.end_index(),       
+            },
             "results": data,
         })
     
@@ -23,8 +25,8 @@ class PostListPagination(CustomPagination):
             page_size = 10
 
 class CommentsListPagination(CustomPagination):
-        page_size = 10
+        page_size = 5
 
 class LikeListPagination(CustomPagination):
-    page_size = 20
+    page_size = 15
 
